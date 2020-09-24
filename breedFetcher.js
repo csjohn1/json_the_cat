0c14a1c335b084ee3499c3f6dad661103d689cca
csjohn1 <csjohn@shaw.ca>
2020-09-24 21:25:24 +0000

    adding mocha chai tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,22 @@
 const request = require('request');
-const fetch = process.argv[2];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${fetch}`, (error, response, body) => {
 
-  if (error) {
-    return console.log(error);
-  }
-  const data = JSON.parse(body);
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+
+    if (error) {
+      return console.log(error);
+    }
+    const data = JSON.parse(body);
   
-  if (data[0]) {
-    return console.log(data[0].description);
-  } else {
-    return console.log('Error: some sort of error likely related to breed name or some such');
-  }
-});
+    if (data[0]) {
+      callback(null, data[0].description);
+      // return console.log(data[0].description);
+    } else {
+      callback('breed not found');
+      // return console.log('Error: some sort of error likely related to breed name or some such');
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
